5702aaecc66695e56c283e0a48285475445502fc
globobit <56482853+globobit@users.noreply.github.com>
2019-10-25 08:10:12 +0300

    Delete model.py

--- 0/model.py
+++
@@ -1,7 +0,0 @@
-model = Pipeline(steps=[
-    ('preprocessor', preprocessor),
-    ('linearregression', LinearRegression())
-])
-
-fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
-num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
